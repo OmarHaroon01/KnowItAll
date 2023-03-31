c8665f4d5c5b3e655f3918100d8653e2f200cee1
momotuuu <momobrb255@gmail.com>
2023-03-31 17:22:23 +0600

    register validation done

--- js/register.js
+++ js/register.js
@@ -1,19 +1,48 @@
-let parent = document.querySelector("#call")
-let save = document.querySelector("#register")
-console.log(save)
+let fullnameInput = document.getElementById("fullnameInput")
+let emailInput = document.getElementById("emailInput")
+let passwordInput = document.getElementById("passwordInput")
+let passwordInput2 = document.getElementById("passwordInput2")
+let errorMessage = document.getElementById("errorMessage")
 
-save.addEventListener("click", async () => {
-    console.log("Hi")
-    // var valid = String(email.value)
-    // .toLowerCase()
-    // .match(
-    //   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    // );
 
-    const res = await fetch("http://localhost:8080/register.php", {
-        method: "POST",
-        body: JSON.stringify({ "name": "Omar"}),
-    });
-    const output = await res.json();
-    console.log(output)
-});
+function removeError(){
+    errorMessage.classList.add("invisible")
+}
+
+function emailValidation(email) {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+function registerButtonClicked() {
+    console.log(fullnameInput.value)
+    console.log(emailInput.value)
+    console.log(passwordInput.value)
+    console.log(passwordInput2.value)
+
+    if (fullnameInput.value == "") {
+        errorMessage.classList.remove("invisible")
+        errorMessage.innerHTML = "Please enter your name first!"
+        return
+    }
+    if (!emailValidation(emailInput.value)) {
+        errorMessage.classList.remove("invisible")
+        errorMessage.innerHTML = "Please enter a valid Email!"
+        return
+    }
+    if (passwordInput.value == "") {
+        errorMessage.classList.remove("invisible")
+        errorMessage.innerHTML = "Please enter your Password!"
+        return
+    }
+    if (passwordInput2.value != passwordInput.value) {
+        errorMessage.classList.remove("invisible")
+        errorMessage.innerHTML = "Passwords don't match"
+        return
+    }
+}
+
+
