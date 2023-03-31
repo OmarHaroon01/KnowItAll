e8c5ce866b60d6060161049189eebd49b1f72be7
OmarHaroon01 <omarharoon01@yahoo.com>
2023-04-01 00:38:30 +0600

    minor bugs fixed

--- backend/auth/register.php
+++ backend/auth/register.php
@@ -8,7 +8,7 @@ $decode = json_decode($input, true);
 $unid = uniqid();
 $fullName = $decode["fullName"];
 $email = $decode["email"];
-$password = $decode["password"];
+$password = password_hash($decode["password"], PASSWORD_DEFAULT);
 
 $sql = "SELECT * FROM knowitall.users WHERE knowitall.users.email= '$email'";
 
@@ -18,11 +18,14 @@ if (mysqli_num_rows($output) !== 0) {
         echo json_encode(["data" => "", "error" => 'Email already registered']);
 } else {
         $sql = "INSERT INTO knowitall.users (unid, fullName, email, password)
- VALUES ($unid, $fullName, $email, $password);";
+ VALUES ('$unid', '$fullName', '$email', '$password');";
 
-        $output = mysqli_query($conn, $sql);
-       // echo json_encode(["data" => "Register Successfully!", "error" => ""]);
-        echo json_encode(["data" => "$output", "error" => ""]);
+
+        if (mysqli_query($conn, $sql)) {
+                echo json_encode(["data" => "Registration Successfull!", "error" => ""]);
+        }
+
+
 
 
 }
--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,7 @@
+let username = document.getElementById("username")
+let quizNumber = document.getElementById("quizNumber")
 
-
+window.onload = preLoad;
 async function preLoad(){
     const res = await fetch("http://localhost:8080/user/dashboard.php", {
         method: "POST",
@@ -7,4 +9,6 @@ async function preLoad(){
     });
     const output = await res.json();
     console.log(output)
+    username.innerHTML = output[0].fullName;
+    quizNumber.innerHTML = output.length;
 }
