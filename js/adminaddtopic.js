62fdddbdec3901c94e08f3364c465dc579643d03
momotuuu <momobrb255@gmail.com>
2023-05-13 11:45:24 +0600

    updated

---
+++ js/adminaddtopic.js
@@ -0,0 +1,14 @@
+var hasReferrer = document.referrer != ""
+
+if (document.referrer == "") {
+    window.location.href = "admin.html";
+
+}
+
+function addTopicButtonClicked() {
+    var topicInput= document.getElementById("topicInputId")
+    console.log(topicInput.value)
+
+    if()
+}
+
